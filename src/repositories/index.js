d8054081ce2564c36f1fe7c666ba2b41b9653e82
henter36 <henter366@gmail.com>
2026-04-29 07:20:34 +0300

    Export Brand Slice 1 repositories

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,9 +1,15 @@
 const { WorkspaceRepository } = require("./workspace-repository");
 const { MembershipRepository } = require("./membership-repository");
 const { RbacRepository } = require("./rbac-repository");
+const { BrandProfileRepository } = require("./brand-profile-repository");
+const { BrandVoiceRuleRepository } = require("./brand-voice-rule-repository");
 
 function createRepositories({ pool }) {
+  const brandProfiles = new BrandProfileRepository({ pool });
+
   return {
+    brandProfiles,
+    brandVoiceRules: new BrandVoiceRuleRepository({ pool, brandProfiles }),
     memberships: new MembershipRepository({ pool }),
     rbac: new RbacRepository({ pool }),
     workspaces: new WorkspaceRepository({ pool }),
@@ -11,6 +17,8 @@ function createRepositories({ pool }) {
 }
 
 module.exports = {
+  BrandProfileRepository,
+  BrandVoiceRuleRepository,
   MembershipRepository,
   RbacRepository,
   WorkspaceRepository,
