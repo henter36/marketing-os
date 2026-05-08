c8904f6ee022cc6e3cde672b42db5ae5913ccb36
henter36 <henter366@gmail.com>
2026-05-08 13:17:58 +0300

    fix: include database diagnostic error metadata

--- src/repositories/repository-error-logging.js
+++ src/repositories/repository-error-logging.js
@@ -3,6 +3,9 @@ function logUnexpectedRepositoryError(repositoryName, error) {
     error_name: error?.name,
     error_message: error?.message,
     error_code: error?.code,
+    error_detail: error?.detail,
+    error_hint: error?.hint,
+    error_constraint: error?.constraint,
     error_stack: error?.stack,
   });
 }
